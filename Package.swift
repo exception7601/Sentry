0befb5a378271d8f784d9c468bea2127a6bc5be0
exception7601 <77030548+exception7601@users.noreply.github.com>
2025-09-22 14:11:11 -0300

    new Version 8.56.0

--- Package.swift
+++ Package.swift
@@ -1,1 +1,24 @@
-// swift-tools-version: 6.0 import PackageDescription let package = Package( name: "Sentry", platforms: [.iOS(.v12)], products: [ .library( name: "Sentry", targets: [ "Sentry", ] ), ], targets: [ .binaryTarget( name: "Sentry", url: "https://github.com/exception7601/Sentry/releases/download/8.56.0/Sentry-51bc92f984da1c36.zip", checksum: "ecc100b59f2044800650f17786c33f071ea580cb318d82e8ce1d403b643555a4" ) ] )
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+  name: "Sentry",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(
+      name: "Sentry",
+      targets: [
+        "Sentry",
+      ]
+    ),
+  ],
+
+  targets: [
+    .binaryTarget(
+      name: "Sentry",
+      url: "https://github.com/exception7601/Sentry/releases/download/8.56.0/Sentry-09cd43e66f8179c8.zip",
+      checksum: "ecc100b59f2044800650f17786c33f071ea580cb318d82e8ce1d403b643555a4"
+    )
+  ]
+)
