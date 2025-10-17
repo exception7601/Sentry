207ab07e8cdf08572748df752c854517fd86df8a
exception7601 <77030548+exception7601@users.noreply.github.com>
2025-10-17 13:49:45 -0300

    new Version 8.57.0

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Sentry",
-      url: "https://github.com/exception7601/Sentry/releases/download/8.56.0/Sentry-0befb5a378271d8f.zip",
-      checksum: "ecc100b59f2044800650f17786c33f071ea580cb318d82e8ce1d403b643555a4"
+      url: "https://github.com/exception7601/Sentry/releases/download/8.57.0/Sentry-0add0b562af8fdc4.zip",
+      checksum: "2e7948d8fac8b7dfbcc24117c2ae02e79757a49ff3dfb15a06866fc405134a38"
     )
   ]
 )
