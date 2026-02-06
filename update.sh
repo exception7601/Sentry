44aa36758d044dae17469971a5972b5d69f5611b
exception7601 <77030548+exception7601@users.noreply.github.com>
2026-02-05 22:06:08 -0300

    update: use sha256sum with shell expansion in update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -6,7 +6,6 @@ NAME=Sentry.xcframework.zip
 REPO=getsentry/sentry-cocoa
 MY_REPO=exception7601/Sentry
 BUILD_COMMIT=$(git log --oneline --abbrev=16 --pretty=format:"%h" -1)
-JSON_FILE="Carthage/SentryBinary.json"
 NEW_NAME=Sentry-${BUILD_COMMIT}.zip
 
 VERSION=$(gh release list \
@@ -33,38 +32,29 @@ gh release download \
 mv $NAME "$NEW_NAME"
 
 DOWNLOAD_URL="https://github.com/${MY_REPO}/releases/download/${VERSION}/${NEW_NAME}"
-
-if [ ! -f $JSON_FILE ]; then
-  echo "{}" > $JSON_FILE
-fi
-
-# Make Carthage
-JSON_CARTHAGE="$(jq --arg version "${VERSION}" --arg url "${DOWNLOAD_URL}" '. + { ($version): $url }' $JSON_FILE)" 
-echo "$JSON_CARTHAGE" > "$JSON_FILE"
+SUM=$(sha256sum "${NEW_NAME}")
+SUM=${SUM%% *}
 
 NOTES=$(cat <<END
-Carthage
-\`\`\`
-binary "https://raw.githubusercontent.com/${MY_REPO}/main/${JSON_FILE}"
-\`\`\`
+SPM binaryTarget
 
-Install
-\`\`\`
-carthage bootstrap --use-xcframeworks
+\`\`\`swift
+.binaryTarget(
+    name: "Sentry",
+    url: "${DOWNLOAD_URL}",
+    checksum: "${SUM}"
+)
 \`\`\`
 END
 )
 echo "${NOTES}"
 
-# BUILD=$(date +%s)
+BUILD=$(date +%s)
 NEW_VERSION=${VERSION}
 
-# echo "$PACKAGE" > Package.swift
-# echo ${NEW_VERSION} > version
-git add "$JSON_FILE"
-git commit -m "new Version ${NEW_VERSION}"
+echo "${NEW_VERSION}.${BUILD}" > version
+git commit -m "new Version ${NEW_VERSION}" --allow-empty
 git tag -a "${NEW_VERSION}" -m "v${NEW_VERSION}"
-# git checkout -b release-v${NEW_VERSION}
 git push origin HEAD --tags
 
 gh release create "${NEW_VERSION}" "${NEW_NAME}" --notes "${NOTES}"
